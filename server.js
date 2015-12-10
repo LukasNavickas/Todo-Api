4f9a2b1e42d9a43c00858b0097e8acf1c2a39064
Lukas Navickas <luk.navickas@gmail.com>
2015-12-10 23:31:40 +0000

    Add PUT todos

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed'); // keep only description and completed on body/post request
 	// console.log('description: ' + body.description);
 	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) { // trim removes empty spaces
-		return res.status(400).send();
+		return res.status(404).send();
 	}
 
 	body.description = body.description.trim();
@@ -60,11 +60,38 @@ app.delete('/todos/:id', function(req, res) {
 	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (!matchedTodo) {
-		res.status(400).json({"error": "No ToDo found with provided id"});
+		res.status(404).json({"error": "No ToDo found with provided id"});
 	} else {
 		todos = _.without(todos, matchedTodo);
 		res.json(matchedTodo);
 	}
+});
+
+app.put('/todos/:id', function(req, res) { // UPDATES TODO LIST
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed'); // keep only description and completed on body/post request
+	var validAttributes = {};
+
+	if(!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(404).send();
+	}
+
+	// HERE WE JUST OVERWRITE OUR ARRAY
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
 });
 
 app.listen(PORT, function() {
