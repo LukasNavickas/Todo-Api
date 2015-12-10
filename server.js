0e9f9dce98af3f54c2262b249429f1036d8dec87
Lukas Navickas <luk.navickas@gmail.com>
2015-12-10 22:53:23 +0000

    use pick to check

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var todos = [];
@@ -18,14 +19,15 @@ app.get('/todos', function(req, res) {
 });
 
 app.get('/todos/:id', function(req, res) {
-	var todoid = parseInt(req.params.id, 10); // always use 10 for normal cases, means nothing...
-	var matchedTodo;
+    var todoid = parseInt(req.params.id, 10); // always use 10 for normal cases, means nothing...
+	var matchedTodo = _.findWhere(todos, {id: todoid}); // give object, where todos.id = :id
+	// var matchedTodo;
 
-	todos.forEach(function(todo) {
-		if(todoid === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	// todos.forEach(function(todo) {
+	// 	if(todoid === todo.id) {
+	// 		matchedTodo = todo;
+	// 	}
+	// });
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -37,8 +39,14 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos
 app.post('/todos', function(req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed'); // keep only description and completed on body/post request
 	// console.log('description: ' + body.description);
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) { // trim removes empty spaces
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+
 	body.id = todoNextId;
 	todoNextId++;
 
