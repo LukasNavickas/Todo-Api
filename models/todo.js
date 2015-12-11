46fef142d2bbab548f09da3658300853a944f26e
Lukas Navickas <luk.navickas@gmail.com>
2015-12-11 14:39:56 +0000

    add user model and POST /users request

--- models/todo.js
+++ models/todo.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('todo', {
+	return sequelize.define('todo', { // create table todo
 		description: {
 			type: DataTypes.STRING,
 			allowNull: false, // validation
