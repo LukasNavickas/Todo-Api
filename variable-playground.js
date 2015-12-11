092588e976bf10ce7387b16fbd54fe4536971d2b
Lukas Navickas <luk.navickas@gmail.com>
2015-12-11 00:18:44 +0000

    add variable-playrgound file

---
+++ variable-playground.js
@@ -0,0 +1,22 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [50, 88];
+
+function addGrades(gradesArr) {
+	//gradesArr.push(55); // prints [50, 88, 55]
+	gradesArr = [50, 88, 55]; // print same result [50, 88]...
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
