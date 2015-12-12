18b3bfa705ec8bae6b50e488ce71969028d293b6
Lukas Navickas <luk.navickas@gmail.com>
2015-12-12 00:55:53 +0000

    del db

--- server.js
+++ server.js
@@ -255,7 +255,7 @@ app.post('/users/login', function(req, res) {
 	
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('express listening on port ' + PORT + '!'); // start server
 	});
